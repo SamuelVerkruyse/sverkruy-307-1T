6b98afa6f9b51882eda08f480bd5f0461a626352
SamuelVerkruyse <samuelverkruyse@gmail.com>
2018-10-05 10:52:23 -0700

    Update

---
+++ src/multiplication/multiplier.java
@@ -0,0 +1,12 @@
+package multiplication;
+
+public class multiplier {
+	
+	public static void main(String args[]) {
+		multi(2,3);
+	}
+	
+	static int multi(int one, int two) {
+		return one * two;
+	}
+}
